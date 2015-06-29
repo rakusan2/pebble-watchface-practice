3d362ef5ce1e5048bb20cda7ef5ced656733fa7f
Tomas Rakusan <Rakusan2@gmail.com>
2015-06-28 23:06:12 -0400

    working simple 4 color digital watch

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <pebble.h>
 static Window *s_m_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void update_time(){
   time_t temp = time(NULL);
@@ -20,11 +23,16 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
 }
 
 static void m_window_load(Window *window){
-  s_time_layer = text_layer_create(GRect(0,55,144,50));
+  s_background_layer=bitmap_layer_create(GRect(0,0,144,168));
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_RED_BLUE_PNG);
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window),bitmap_layer_get_layer(s_background_layer));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_DOS_VGA_48));
+  s_time_layer = text_layer_create(GRect(5,52,139,50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer,s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   update_time();
@@ -32,6 +40,9 @@ static void m_window_load(Window *window){
 
 static void m_window_unload(Window *window){
   text_layer_destroy(s_time_layer);
+  fonts_unload_custom_font(s_time_font);
+  gbitmap_destroy(s_background_bitmap);
+  bitmap_layer_destroy(s_background_layer);
 }
 
 static void init(){
